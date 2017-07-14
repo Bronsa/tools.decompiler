316006e9ce568865f6735a23945b992e773acbf0
Nicola Mometto <brobronsa@gmail.com>
2017-07-15 00:49:12 +0100

    wip

--- src/clojure/tools/decompiler/bc.clj
+++ src/clojure/tools/decompiler/bc.clj
@@ -1,6 +1,53 @@
 (ns clojure.tools.decompiler.bc
   (:require [clojure.java.io :as io])
-  (:import (org.apache.bcel.classfile ClassParser JavaClass)))
+  (:import (org.apache.bcel.classfile ClassParser JavaClass Field AccessFlags)))
 
-(defn classfile->bytecode ^JavaClass [filename]
-  (-> filename (ClassParser.) .parse))
+(set! *warn-on-reflection* true)
+
+(defn parse-classfile ^JavaClass [filename]
+  (-> filename
+      (ClassParser.)
+      (.parse)))
+
+(defn class-interfaces [^JavaClass klass]
+  (->> klass
+       (.getAllInterfaces)
+       (mapv #(.getClassName ^JavaClass %))))
+
+(defn parse-flags [^AccessFlags flags]
+  (cond-> #{}
+    (.isAbstract flags) (conj :abstract)
+    (.isFinal flags) (conj :final)
+    (.isInterface flags) (conj :interface)
+    (.isPrivate flags) (conj :private)
+    (.isProtected flags) (conj :protected)
+    (.isPublic flags) (conj :public)
+    (.isStatic flags) (conj :static)
+    (.isSynchronized flags) (conj :synchronized)
+    (.isSynthetic flags) (conj :synthetic)
+    (.isVolatile flags) (conj :volatile)))
+
+(defn parse-field [^Field field]
+  (let [type (-> field
+                 (.getType)
+                 (str))
+        name (.getName field)]
+    {:class.field/name name
+     :class.field/class type
+     :class.field/flags (parse-flags field)}))
+
+(defn class-fields [^JavaClass klass]
+  (->> klass
+       (.getFields)
+       (mapv parse-field)))
+
+(defn analyze-classfile [filename]
+  (let [klass (parse-classfile filename)]
+    {:class/name (.getClassName klass)
+     :class/interfaces (class-interfaces klass)
+     :class/super (-> klass (.getSuperClass) (.getClassName))
+     :class/filename (.getSourceFileName klass)
+     :class/type (if (.isClass klass)
+                   :class
+                   :interface)
+     :class/fields (class-fields klass)}))
