2f2cb4c13e7b0ad3381f0f80010d587e82481160
Nicola Mometto <brobronsa@gmail.com>
2017-07-15 19:09:23 +0100

    wip

--- src/clojure/tools/decompiler/source.clj
+++ src/clojure/tools/decompiler/source.clj
@@ -6,13 +6,33 @@
                   :local-variable-table {}
                   :ast {}})
 
-; bc, ctx -> ctx
+(defn process-insn [ctx insn]
+  ctx)
+
+(defn process-insns [ctx bytecode]
+  (reduce process-insn ctx bytecode))
+
+;; bc, ctx -> ctx
 (defn static-init [{:class/keys [methods] :as bc} ctx]
-  (let [init (u/find-method methods {:method/name "<clinit>"})]
-    ctx))
+  (let [{:method/keys [bytecode]} (u/find-method methods {:method/name "<clinit>"})]
+    (process-insns ctx bytecode)))
 
 (defn init [{:class/keys [methods] :as bc} ctx]
-  ctx)
+  (let [{:method/keys [bytecode]} (u/find-method methods {:method/name "<init>"})]
+    (process-insns ctx bytecode)))
+
+(defn decompile-fn-methods [bc ctx]
+  (let [invokes (u/find-methods methods {:method/name "invoke"})
+        invokes-static (u/find-methods methods {:method/name "invokeStatic"})
+        invoke-methods (into invoke-static (for [{:method/keys [arg-types] :as invoke} invokes
+                                                 :let [argc (count arg-types)]
+                                                 :when (empty? (filter (fn [:method/keys [arg-types]]
+                                                                         (= (count arg-types) argc))
+                                                                       invoke-static))]))
+        methods-asts (map (partial decompile-fn-method ctx) invoke-methods)]
+    (assoc ctx
+           :ast {:op :fn
+                 :fn-methods [method-asts]})))
 
 (defn decompile-fn [{class-name :class/name
                      :class/keys [methods] :as bc}
@@ -23,7 +43,8 @@
 
     (->> ctx
          (static-init bc)
-         (init bc))))
+         (init bc)
+         (decompile-fn-methods bc))))
 
 (defn bc->ast [{:class/keys [super] :as bc}]
   (if (#{"clojure.lang.AFunction" "clojure.lang.RestFn"} super)
@@ -38,4 +59,7 @@
   (def bc (bc/analyze-classfile filename))
 
   (bc->ast bc)
+
+  (fn* ([] "yoo"))
+
   )
