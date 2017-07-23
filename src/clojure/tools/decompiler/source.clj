0a55e7715bbc6f6de7b5e03f5701ce697ff6832a
Nicola Mometto <brobronsa@gmail.com>
2017-07-23 11:37:10 +0100

    why was this here?

--- src/clojure/tools/decompiler/source.clj
+++ src/clojure/tools/decompiler/source.clj
@@ -19,8 +19,7 @@
           ~(->> (for [[type match test expr] exprs]
                   (if (= :collision type)
                     [match [match (ast->clj expr)]]
-                    ;; sorry
-                    [match [(eval (ast->clj test)) (ast->clj expr)]]))
+                    [match [(ast->clj test) (ast->clj expr)]]))
                (into (sorted-map)))
           ~type ~switch-type ~@(when skip-check [skip-check])))
 
