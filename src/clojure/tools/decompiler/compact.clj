06bc44cd763b38a1c8fbb7ac76152bed3f7b87bb
Nicola Mometto <brobronsa@gmail.com>
2017-07-24 10:57:18 +0100

    wip core match backed prototype

--- src/clojure/tools/decompiler/compact.clj
+++ src/clojure/tools/decompiler/compact.clj
@@ -7,114 +7,71 @@
 ;;   You must not remove this notice, or any other, from this software.
 
 (ns clojure.tools.decompiler.compact
-  (:require [clojure.core.logic :as l]
-            [clojure.core.logic.unifier :as u]
-            [clojure.core.match :refer [match]]
+  (:require [clojure.core.match :as m]
             [clojure.walk :as w]))
 
-;; ;; Adapted from kibit
-;; (defn macrocompact-step [expr rules]
-;;   (let [alts (l/run 1 [q]
-;;                (l/fresh [pat subst]
-;;                  (l/membero [pat subst] rules)
-;;                  (l/project [pat subst]
-;;                             (l/all (pat expr)
-;;                                    (subst q)))))]
-;;     (if (empty? alts) expr (first alts))))
+(defn register! [sym !occurs]
+  (if (contains? @!occurs sym)
+    (let [s (gensym (str (name sym) "_"))]
+      (swap! !occurs update sym conj s)
+      s)
+    (do
+      (swap! !occurs assoc sym #{})
+      sym)))
 
-;; (defn compile-rule [rule]
-;;   `(let [[pat# alt#] (u/prep '~rule)]
-;;      [(fn [expr#] (l/== expr# pat#))
-;;       (fn [sbst#] (l/== sbst# alt#))]))
+(defn compile-pattern [form !occurs]
+  (cond
 
-;; (defn raw-rule? [rule]
-;;   (not (vector? rule)))
+    (seq? form)
+    [(list (vec (mapcat #(compile-pattern % !occurs) form)) :seq)]
 
-;; (defmacro defrules [name & rules]
-;;   `(def ~name
-;;      ~(->> (for [rule rules]
-;;              (if (raw-rule? rule)
-;;                rule
-;;                (compile-rule rule)))
-;;            (into []))))
+    (vector? form)
+    [(vec (mapcat #(compile-pattern % !occurs) form))]
 
-;; (defrules rules
-;;   [(let* ?binds . ?body) (clojure.core/let ?binds . ?body)]
-;;   [(if ?test (do . ?then)) (clojure.core/when ?test . ?then)]
-;;   [(if ?test ?then nil) (clojure.core/when ?test ?then)]
+    (map? form)
+    [(reduce-kv (fn [m k v] (assoc m (compile-pattern k !occurs) (compile-pattern v !occurs))) {} form)]
 
-;;   [(clojure.lang.Var/pushThreadBindings ?binds) (clojure.core/push-thread-bindings ?binds)]
-;;   [(clojure.lang.Var/popThreadBindings) (clojure.core/pop-thread-bindings)]
+    (symbol? form)
 
-;;   [(do (clojure.core/push-thread-bindings ?binds)
-;;        (try
-;;          ?body
-;;          (finally (clojure.core/pop-thread-bindings))))
-;;    (clojure.core/with-bindings ?binds ?body)]
+    (if (= \? (first (name form)))
+      (if (= \& (second (name form)))
+        ['& (register! form !occurs)]
+        [(register! form !occurs)])
+      [(list 'quote form)])
 
-;;   [(clojure.core/with-bindings
-;;      {clojure.lang.Compiler/LOADER ?_}
-;;      . ?body)
-;;    (do . ?body)]
+    :else
+    [form]))
 
-;;   [(if (.equals ?ns 'clojure.core) nil (do (clojure.lang.LockingTransaction/runInTransaction . ?fn) nil))   nil]
+(defn assert-unify [patterns]
+  (list* `and
+         (for [[bind unifiers] patterns]
+           `(= ~bind ~@unifiers))))
 
-;;   [(clojure.core/let [?x ?y]
-;;      (clojure.core/when ?x
-;;        (clojure.core/let [?z ?x] . ?body)))
-;;    (clojure.core/when-let [?z ?y] . ?body)]
+(def backtrack-all (Exception.))
 
-;;   [(loop* . ?l) (clojure.core/loop . ?l)]
+(defn compile-patterns [patterns]
+  (->> (for [[pattern -> replacement] patterns
+             :let [!occurs (atom {})]]
+         [(compile-pattern pattern !occurs) `(if ~(assert-unify @!occurs)
+                                               ~replacement
+                                               (throw backtrack-all))])
+       (mapcat identity)))
 
-;;   (let [[?seq ?b ?chunk ?count ?i ?body ?c ?a ?c ?_] (repeatedly l/lvar)]
-;;     [#(l/== % `(loop [~?seq (seq ~?b) ~?chunk nil ~?count 0 ~?i 0]
-;;                  (if (< ~?i ~?count)
-;;                    (let [~?a (.nth ~?chunk ~?i)]
-;;                      ~(l/llist 'do ?body))
-;;                    (when-let [~?seq (seq ~?seq)]
-;;                      (if (chunked-seq? ~?seq)
-;;                        (let [~?c (chunk-first ~?seq)]
-;;                          (recur (chunk-rest ~?seq) ~?c (count ~?c) 0))
-;;                        ~(l/llist `let [?a (list `first ?seq)] ?_))))))
-;;      #(l/project [?a ?b ?body]
-;;                  (l/== % `(clojure.core/doseq [~?a ~?b] ~@(butlast ?body))))])
-
-;;   [(clojure.core/let [?x ?y]
-;;      (if ?x
-;;        (clojure.core/let [?z ?x] . ?body)
-;;        ?else))
-;;    (clojure.core/if-let [?z ?y] (do . ?body) ?else)]
-
-;;   [(fn* . ?body) (clojure.core/fn . ?body)]
-
-;;   [((clojure.core/fn ?n ([] . ?body))) (do . ?body)]
-
-;;   (let [?body1 (l/lvar)
-;;         ?body2 (l/lvar)]
-;;     [#(l/== % (l/llist 'do (l/llist 'do ?body1) ?body2))
-;;      #(l/project [?body1 ?body2]
-;;                  (l/== % `(do ~@?body1 ~@?body2)))])
-
-;;   [(.setMeta ?ref ?meta) (clojure.core/reset-meta! ?ref ?meta)]
-;;   [(clojure.core/reset-meta! ?var {:arglists ?_ :line ?__ :column ?___ :file ?____}) nil]
-
-;;   [(.withMeta (clojure.core/list . ?body) {:line ?_ :column ?__}) (clojure.core/list . ?body)]
-
-;;   [(.bindRoot (var ?var) (clojure.core/fn ?name . ?body))
-;;    (clojure.core/defn ?name . ?body)]
-
-;;   (let [?argv1 (l/lvar)
-;;         ?argv2 (l/lvar)
-;;         ?body (l/lvar)]
-;;     [#(l/== % (list `let ?argv1 (l/llist `let ?argv2 ?body)))
-;;      #(l/project [?argv1 ?argv2 ?body]
-;;                  (l/== % `(let [~@?argv1 ~@?argv2] ~@?body)))]))
+(defmacro compact [expr & patterns]
+  `(let [expr# ~expr]
+     (try
+       (m/match [expr#]
+         ~@(compile-patterns patterns)
+         :else expr#)
+       (catch Exception e#
+         (if (identical? e# backtrack-all)
+           expr#
+           (throw e#))))))
 
 (defn macrocompact-step [expr]
-  (match [expr]
-         [(['fn* & ?body] :seq)] `(fn ~@?body)
-         [([`let [?a ?b] ([`let ?binds & ?body] :seq)] :seq)] `(let [~?a ~?b ~@?binds] ~@?body)
-         :else expr))
+  (compact expr
+    [(`let [?a ?b] (`let ?binds ?&body)) -> `(let [~?a ~?b ~@?binds] ~@?&body)]
+    [(fn* ?&body) -> `(fn ~@?&body)]))
 
 (defn macrocompact [source]
   (w/postwalk
