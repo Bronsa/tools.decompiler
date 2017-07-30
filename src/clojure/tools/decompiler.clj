1fb4f177f41b3a32a54b8117c0cea70cc7d6a25a
Nicola Mometto <brobronsa@gmail.com>
2017-07-30 09:17:03 +0100

    move pprint to a separate ns

--- src/clojure/tools/decompiler.clj
+++ src/clojure/tools/decompiler.clj
@@ -9,64 +9,17 @@
 (ns clojure.tools.decompiler
   (:require [clojure.java.io :as io]
             [clojure.string :as s]
-            [clojure.walk :as w]
             [clojure.tools.decompiler.bc :as bc]
             [clojure.tools.decompiler.ast :as ast]
             [clojure.tools.decompiler.sugar :as sa]
             [clojure.tools.decompiler.source :as src]
             [clojure.tools.decompiler.compact :as cmp]
-            [fipp.clojure :as fp]))
+            [clojure.tools.decompiler.pprint :as pp]))
 
 (defn absolute-filename [filename]
   (-> filename
       (io/file)
       (.getAbsolutePath)))
-
-(defn ->pprint-str [source]
-  (with-out-str (fp/pprint source {:width 100})))
-
-(defn elide-ns [source]
-  (let [!aliases (atom {})
-        w (fn [f s] (w/walk f identity s))
-        f (fn f [x]
-            (doto (if (seq? x)
-                    (if (= 'quote (first x))
-                      x
-                      (do
-                        (when (and (= 'clojure.core/in-ns (first x))
-                                   (seq? (second x))
-                                   (= 'quote (-> x second first)))
-                          (let [ns (-> x second second)
-                                ns (if (list? ns) (second ns) ns)]
-                            (swap! !aliases assoc (name ns) "")))
-                        (when (= 'clojure.core/refer-clojure (first x))
-                          (let [ex (->> x (drop-while (complement #{:exclude})) second)]
-                            (when (vector? ex)
-                              (swap! !aliases assoc "clojure.core" (->> ex (map (comp str second)) (into #{}))))
-                            (when (list? ex)
-                              ;; assumes withMeta
-                              (swap! !aliases assoc "clojure.core" (->> ex second rest (map (comp str second)) (into #{}))))))
-                        (when (= 'clojure.core/require (first x))
-                          (doseq [req (rest x)
-                                  :when (vector? req)]
-                            (when-let [alias (some->> req (drop-while (complement #{:as})) second second name)]
-                              (let [ns (some-> req first second name)]
-                                (when-not (= ns "clojure.core")
-                                  (swap! !aliases assoc ns alias))))))
-                        (w f x)))
-                    (if (symbol? x)
-                      (let [aliases @!aliases]
-                        (if (= "clojure.core" (namespace x))
-                          (if (contains? (get aliases "clojure.core") (name x))
-                            x
-                            (symbol (name x)))
-                          (if-let [alias (get aliases (namespace x))]
-                            (if (= "" alias)
-                              (symbol (name x))
-                              (symbol (str alias "/" (name x))))
-                            x)))
-                      (w f x)))))]
-    (w f source)))
 
 (defn classfile->source [filename bc-for]
   (-> filename
@@ -78,8 +31,7 @@
       (cmp/macrocompact)
       (->> (keep identity)
            (remove #(and (seq? %) (= 'var (first %)))))
-      (elide-ns)
-      (->pprint-str)))
+      (pp/pprint)))
 
 (defn cname [c input-path]
   (-> c
---
+++ src/clojure/tools/decompiler/pprint.clj
@@ -0,0 +1,66 @@
+;;   Copyright (c) Nicola Mometto & contributors.
+;;   The use and distribution terms for this software are covered by the
+;;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;;   which can be found in the file epl-v10.html at the root of this distribution.
+;;   By using this software in any fashion, you are agreeing to be bound by
+;;   the terms of this license.
+;;   You must not remove this notice, or any other, from this software.
+
+(ns clojure.tools.decompiler.pprint
+  (:require [clojure.walk :as w]
+            [fipp.clojure :as fp]))
+
+(defn ->pprint-str [source]
+  (with-out-str (fp/pprint source {:width 100})))
+
+(defn elide-ns [source]
+  (let [!aliases (atom {})
+        w (fn [f s] (w/walk f identity s))
+        f (fn f [x]
+
+            (cond
+              (seq? x)
+              (if (= 'quote (first x))
+                x
+                (do
+                  (when (and (= 'clojure.core/in-ns (first x))
+                             (seq? (second x))
+                             (= 'quote (-> x second first)))
+                    (let [ns (-> x second second)
+                          ns (if (list? ns) (second ns) ns)]
+                      (swap! !aliases assoc (name ns) "")))
+
+                  (when (= 'clojure.core/refer-clojure (first x))
+                    (let [ex (->> x (drop-while (complement #{:exclude})) second)
+                          ex (if (vector? ex) ex (rest ex))]
+                      (swap! !aliases assoc "clojure.core" (->> ex (map (comp str second)) (into #{})))))
+
+                  (when (= 'clojure.core/require (first x))
+                    (doseq [req (rest x)
+                            :when (vector? req)]
+                      (when-let [alias (some->> req (drop-while (complement #{:as})) second second name)]
+                        (let [ns (some-> req first second name)]
+                          (when-not (= ns "clojure.core")
+                            (swap! !aliases assoc ns alias))))))
+                  (w f x)))
+
+              (symbol? x)
+              (let [aliases @!aliases]
+                (if (= "clojure.core" (namespace x))
+                  (if (contains? (get aliases "clojure.core") (name x))
+                    x
+                    (symbol (name x)))
+                  (if-let [alias (get aliases (namespace x))]
+                    (if (= "" alias)
+                      (symbol (name x))
+                      (symbol (str alias "/" (name x))))
+                    x)))
+
+              :else
+              (w f x)))]
+    (w f source)))
+
+(defn pprint [source]
+  (-> source
+      (elide-ns)
+      (->pprint-str)))
