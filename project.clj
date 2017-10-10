5052998701cdb253c8f0c9794d1cfe0c38fdecc6
Nicola Mometto <brobronsa@gmail.com>
2017-10-10 22:57:16 +0100

    update deps.edn/project.clj

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :java-source-paths ["java"]
   :manifest {"Premain-Class" "clojure.tools.decompiler.RetrieveClasses"}
-  :dependencies [[org.clojure/clojure "1.9.0-beta1"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta2"]
                  [org.clojure/core.match "0.3.0-alpha5"]
                  [org.apache.bcel/bcel "6.1"]
                  [fipp "0.6.10"]])
