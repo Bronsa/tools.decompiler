4a5f92af13bb386381053ab6be88acc762f766cc
Nicola Mometto <brobronsa@gmail.com>
2017-10-13 22:58:36 +0100

    add group to project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tools.decompiler "0.1.0-SNAPSHOT"
+(defproject bronsa/tools.decompiler "0.1.0-alpha1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
