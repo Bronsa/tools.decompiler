620b40bbce264fffed1e4cb3ef3209a7a5778106
Nicola Mometto <nicola@aestheticintegration.com>
2017-11-13 10:37:33 +0000

    bump deps

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :java-source-paths ["java"]
   :manifest {"Premain-Class" "clojure.tools.decompiler.RetrieveClasses"}
-  :dependencies [[org.clojure/clojure "1.9.0-beta2"]
+  :dependencies [[org.clojure/clojure "1.9.0-RC1"]
                  [org.clojure/core.match "0.3.0-alpha5"]
                  [org.apache.bcel/bcel "6.1"]
-                 [fipp "0.6.10"]])
+                 [fipp "0.6.12"]])
