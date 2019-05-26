db8864ab64aad1a12a69ce13afcf1cfb040c2058
Zaoqi <zaomir@outlook.com>
2019-05-26 18:03:25 +0800

    Update project.clj

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :java-source-paths ["java"]
   :manifest {"Premain-Class" "clojure.tools.decompiler.RetrieveClasses"}
-  :dependencies [[org.clojure/clojure "1.10.0-master-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/core.match "0.3.0-alpha5"]
                  [org.apache.bcel/bcel "6.1"]
                  [fipp "0.6.12"]])
